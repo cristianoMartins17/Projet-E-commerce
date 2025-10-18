56d3e5338129a6a13d31c9783654325e97eabbd3
SofianeTacherift <sofiane.t09@icloud.com>
2025-10-18 11:42:55 +0200

    Ajout de contenu sur la page à propos

--- A propos/propos.js
+++ A propos/propos.js
@@ -1,7 +1,15 @@
 window.addEventListener("scroll", () => {
   const hauteurActuelle = window.scrollY;
+  const hauteurFenetre = window.innerHeight;
+  const hauteurTotale = document.documentElement.scrollHeight;
   const p = document.getElementById("reponse");
-  p.textContent = hauteurActuelle;
+
+  if (hauteurActuelle + hauteurFenetre >= hauteurTotale) {
+    p.textContent = "ouiii";
+  } else {
+    p.textContent = "";
+  }
+
 });
 
 
