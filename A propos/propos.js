b4a872987c14f8f65c0d62a01d037f37aa136af2
SofianeTacherift <sofiane.t09@icloud.com>
2025-10-18 18:10:32 +0200

    modif a propos

--- A propos/propos.js
+++ A propos/propos.js
@@ -1,15 +1,17 @@
 window.addEventListener("scroll", () => {
-  const hauteurActuelle = window.scrollY;
+  const hauteurScroll = window.scrollY;
   const hauteurFenetre = window.innerHeight;
   const hauteurTotale = document.documentElement.scrollHeight;
-  const p = document.getElementById("reponse");
-
-  if (hauteurActuelle + hauteurFenetre >= hauteurTotale) {
-    p.textContent = "ouiii";
-  } else {
-    p.textContent = "";
+  const main = document.querySelector("main");
+  const hauteurActuelle=hauteurFenetre+hauteurScroll;
+  const footer=document.querySelector("footer")
+  if (hauteurActuelle>= hauteurTotale*9/10) { 
+    const coeff=hauteurActuelle/hauteurTotale;
+    main.style.transform=`rotate(${coeff*-15}deg)`;
   }
-
+  else {
+    main.style.transform="rotate(0deg)";
+  }
 });
 
 
