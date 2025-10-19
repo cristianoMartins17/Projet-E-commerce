fbe4a03114c8b4e93f3500f8961bfc2f87b0f149
SofianeTacherift <sofiane.t09@icloud.com>
2025-10-19 15:09:57 +0200

    ajout style sur a propos

--- A propos/propos.js
+++ A propos/propos.js
@@ -1,17 +1,35 @@
+
+function placeFooter() {
+  const main=document.querySelector("main");
+  const footer=document.querySelector("footer");
+  const body=document.querySelector("body");
+  const tailleFooter=footer.offsetHeight;
+  const tailleMain=main.offsetHeight;
+  footer.style.marginTop=`${-tailleFooter*2/3}px`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  placeFooter();
+});
+
+
 window.addEventListener("scroll", () => {
   const hauteurScroll = window.scrollY;
   const hauteurFenetre = window.innerHeight;
   const hauteurTotale = document.documentElement.scrollHeight;
   const main = document.querySelector("main");
   const hauteurActuelle=hauteurFenetre+hauteurScroll;
-  const footer=document.querySelector("footer")
-  if (hauteurActuelle>= hauteurTotale*9/10) { 
-    const coeff=hauteurActuelle/hauteurTotale;
-    main.style.transform=`rotate(${coeff*-15}deg)`;
+  const footer=document.querySelector("footer");
+  let hauteurEffet=0.9;
+  if (hauteurActuelle>= hauteurTotale*hauteurEffet) {
+    let coeff=(hauteurActuelle-hauteurTotale*hauteurEffet)/(hauteurTotale*(1-hauteurEffet));
+    coeff=Math.min(coeff,1);
+    main.style.transform=`rotate(${-coeff*25}deg)`;
   }
   else {
     main.style.transform="rotate(0deg)";
   }
 });
+
 
 
