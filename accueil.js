21f9cbdc317958f6e9602e10d839df7f62041804
Cristiano Martins <cristiano.martins25@orange.fr>
2025-10-23 21:01:51 +0200

    Implement navbar scroll effect and burger menu toggle
    Added functionality for a burger menu toggle and navbar scroll effect.

--- accueil.js
+++ accueil.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navbar = document.getElementById('mainNav');
 
+  // Changer le style de la navbar quand on scrolle
   // si scrollY > 60 => on ajoute navbar--scrolled
   window.addEventListener('scroll', function () {
     if (!navbar) return;
@@ -13,3 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+// menu burger
+
+    const btn = document.getElementById('burger-btn');
+    const menu = document.getElementById('menu');
+
+    // Quand on clique sur le bouton, on ajoute/enlève la classe "open"
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('open');
+    });
